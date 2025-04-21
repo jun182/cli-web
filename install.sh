c1ac87a54c9ccffb55065e322137a90cd783b842
John <mikael.johnny@gmail.com>
2025-04-21 22:43:49 +0200

    new

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 declare -A scripts=(
-    ["1. Disable AWDL (Example)"]="https://raw.githubusercontent.com/jun182/awdl_disable/main/bin/awdl_disable.sh"
-    ["2. Run Another Script (Replace)"]="https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_OTHER_REPO/main/another_script.sh" # <- Replace with your info
+["1. Disable AWDL (Example)"]="https://raw.githubusercontent.com/jun182/awdl_disable/main/bin/awdl_disable.sh"
+["2. Run Another Script (Replace)"]="https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_OTHER_REPO/main/another_script.sh" # <- Replace with your info
 # Add more options here following the same pattern
 # ["3. Install Docker"]="https://get.docker.com" # <- You could even link to other installers if they support this pattern
 )
