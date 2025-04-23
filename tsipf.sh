6019707fcad790f11f039e44dd1fbe3c81a9bdab
John <mikael.johnny@gmail.com>
2025-04-23 13:28:33 +0200

    update

--- tsipf.sh
+++ tsipf.sh
@@ -6,8 +6,7 @@ sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
 
 sudo apt install networkd-dispatcher -y
 
-NETDEV=$(ip -o route get 8.8.8.8 | cut -f 5 -d " ")
-sudo ethtool -K $NETDEV rx-udp-gro-forwarding on rx-gro-list off
+NETDEV=$(ip -o route get 8.8.8.8 | cut -f 5 -d " ") && sudo ethtool -K $NETDEV rx-udp-gro-forwarding on rx-gro-list off
 
 printf '#!/bin/sh\n\nethtool -K %s rx-udp-gro-forwarding on rx-gro-list off \n' "$(ip -o route get 8.8.8.8 | cut -f 5 -d " ")" | sudo tee /etc/networkd-dispatcher/routable.d/50-tailscale
 sudo chmod 755 /etc/networkd-dispatcher/routable.d/50-tailscale
